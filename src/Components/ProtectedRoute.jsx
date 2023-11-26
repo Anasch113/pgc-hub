cff841cb81f9cdc410d593613c2ddd451adaf5ab
Anasch113 <120164983+Anasch113@users.noreply.github.com>
2023-11-26 23:12:08 +0500

    Update ProtectedRoute.jsx

--- src/Components/ProtectedRoute.jsx
+++ src/Components/ProtectedRoute.jsx
@@ -2,11 +2,7 @@ import React from 'react'
 import { Navigate } from 'react-router-dom';
 import { useUserAuth } from '../context/UserAuthContext';
 
-const ProtectedRoute = ({children}) => {
-  let{ user }= useUserAuth();
-  if(!user){
-return <Navigate to= "/portal"/>
-  }
+//sensitive data
 
   return children;
 }
